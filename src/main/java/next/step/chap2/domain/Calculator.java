b943cd42729b01478ee530db207976b2320ff194
sk1737030 <sk1737030@naver.com>
2021-05-12 23:45:29 +0900

    feat(java-next-step): 2.3.1 요구사항 1단계 구현

--- src/main/java/next/step/chap2/domain/Calculator.java
+++ src/main/java/next/step/chap2/domain/Calculator.java
@@ -1,5 +1,8 @@
 package next.step.chap2.domain;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
     int add(int i, int j) {
         return i + j;
@@ -15,5 +18,23 @@ public class Calculator {
 
     int divide(int i, int j) {
         return i / j;
+    }
+
+    public int add(String values) {
+        Pattern p = Pattern.compile("[^(,|:)]*");
+        Matcher m = p.matcher(values);
+
+        int result = 0;
+
+        if (values.isEmpty())
+            return result;
+
+        while (m.find()) {
+            String group = m.group();
+            if (!group.isBlank())
+                result += Integer.parseInt(group);
+        }
+
+        return result;
     }
 }
--- src/test/java/next/step/chap2/domain/CalculatorTest.java
+++ src/test/java/next/step/chap2/domain/CalculatorTest.java
@@ -1,7 +1,14 @@
 package next.step.chap2.domain;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,18 +17,37 @@ class CalculatorTest {
 
     private Calculator cal;
 
+
+
     @BeforeEach
     public void setUp() {
         cal = new Calculator();
     }
 
-    @Test
-    void add() {
-        assertEquals(9, cal.add(6, 3));
+    @ParameterizedTest(name = "문자열을_쉼표_또는_콜론_구분자를_기준으로_분리한_값_{0}의_합을_반환한다")
+    @MethodSource("numberSet")
+    void 문자열을_쉼표_또는_콜론_구분자를_기준으로_분리한_각_n의_합을_반환한다(int answer, String values) {
+        assertEquals(cal.add(values), answer);
+        assertEquals(cal.add(values), answer);
+        assertEquals(cal.add(values), answer);
+        assertEquals(cal.add(values), answer);
+    }
+
+    private static List<Arguments> numberSet() {
+        return Arrays.asList(
+                Arguments.of(0, ""),
+                Arguments.of(3, "1,2"),
+                Arguments.of(6, "1,2,3"),
+                Arguments.of(6, ",1,2:3")
+        );
     }
 
     @Test
     void subtract() {
         assertEquals(3, cal.subtract(6, 3));
+    }
+
+    @AfterEach
+    public void after() {
     }
 }
