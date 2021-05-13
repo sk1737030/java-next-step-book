c193f95b053a297684a3e080c0102b703448516d
sk1737030 <sk1737030@naver.com>
2021-05-13 23:19:50 +0900

    feat(java-next-step): 문자열계산기 테스트 리팩토링

--- src/test/java/next/step/chap2/domain/StringCalculatorTest.java
+++ src/test/java/next/step/chap2/domain/StringCalculatorTest.java
@@ -1,7 +1,6 @@
 package next.step.chap2.domain;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -21,7 +20,7 @@ class StringCalculatorTest {
         cal = new StringCalculator();
     }
 
-    @ParameterizedTest(name = "문자열을_쉼표_또는_콜론_구분자를_기준으로_분리한_값_{0}의_합을_반환한다")
+    @ParameterizedTest(name = "문자열을 쉼표 또는 콜론 구분자를 기준으로 분리한 값 {0}의 합을 반환한다")
     @MethodSource("numberSet")
     void 문자열을_쉼표_또는_콜론_구분자를_기준으로_분리한_각_n의_합을_반환한다(int answer, String values) {
         assertEquals(cal.add(values), answer);
@@ -37,10 +36,17 @@ class StringCalculatorTest {
         );
     }
 
-    @Test
-    void _커스텀_구분자를_포함한_값을_입력_할_경우_구분자는_세미콜론이며_결과_값은_6이_반환되어야한다() {
-        assertEquals(cal.add("//;\n1;2;3"), 6);
-        assertEquals(cal.add("//;\n1;2//;\n3"), 6);
+    @ParameterizedTest(name = "커스텀 구분자를 포함한 값을 입력 할 경우 이 {0}의 합이 반환되어야한다")
+    @MethodSource("numberSet2")
+    void _커스텀_구분자를_포함한_N_입력_할_경우_구분자는_세미콜론이며_결과_n이_반환되어야한다(int answer, String values) {
+        assertEquals(answer, cal.add(values));
+    }
+
+    private static List<Arguments> numberSet2() {
+        return Arrays.asList(
+                Arguments.of(6, "//;\n1;2;3"),
+                Arguments.of(6, "//;\n1;2//;\n3")
+        );
     }
 
     @ParameterizedTest(name = "문자열 계산기에 {0}을 넣으면 RunstimeException 처리가 되어야한다")
