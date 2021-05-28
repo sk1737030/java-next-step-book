05b3f60a5b3e25da3a6bdc640bb5e0d94f7aafe7
sk1737030 <sk1737030@naver.com>
2021-05-28 19:44:44 +0900

    SET(java-next-step-chap4): 라이브러리 버전 수정

--- src/test/java/next/WebServerLauncher.java
+++ src/test/java/next/WebServerLauncher.java
@@ -1,10 +1,10 @@
 package next;
 
-import java.io.File;
-
 import org.apache.catalina.startup.Tomcat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
 
 public class WebServerLauncher {
     private static final Logger logger = LoggerFactory.getLogger(WebServerLauncher.class);
@@ -13,7 +13,7 @@ public class WebServerLauncher {
         String webappDirLocation = "webapp/";
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8080);
-
+        tomcat.getConnector();
         tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
         logger.info("configuring app with basedir: {}", new File("./" + webappDirLocation).getAbsolutePath());
 
