6fe4b89361f84e544211e5c4342f42836eafedeb
sk1737030 <sk1737030@naver.com>
2021-06-08 22:26:57 +0900

    refactoring(java-next-step-chap6): try with resources 적용

--- src/main/java/next/dao/JdbcTemplate.java
+++ src/main/java/next/dao/JdbcTemplate.java
@@ -11,105 +11,49 @@ import java.util.List;
 
 public abstract class JdbcTemplate {
 
-    public List<?> query(String sql) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
+    public List<?> query(String sql) {
         List<Object> objects = new ArrayList<>();
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
-            rs = pstmt.executeQuery();
-
+        try (Connection con = ConnectionManager.getConnection(); PreparedStatement pstmt = con.prepareStatement(sql); ResultSet rs = pstmt.executeQuery()) {
             while (rs.next()) {
                 objects.add(mapRow(rs));
             }
             return objects;
         } catch (SQLException throwables) {
-            throwables.printStackTrace();
             throw new IllegalArgumentException("잘못된 sql입니다.");
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
         }
     }
 
-    public Object executeQuery(String sql) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
+    public Object executeQuery(String sql) {
+        try (Connection con = ConnectionManager.getConnection(); PreparedStatement pstmt = con.prepareStatement(sql)) {
             setParameters(pstmt);
-
-            rs = pstmt.executeQuery();
-
-            Object user = null;
-            if (rs.next()) {
-                user = mapRow(rs);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                Object user = null;
+                if (rs.next()) {
+                    user = mapRow(rs);
+                }
+                return user;
             }
 
-            return user;
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException sqlException) {
             throw new IllegalArgumentException("잘못된 sql입니다.");
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
         }
     }
 
-    public int update(String sql) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
+    public int update(String sql) {
+        try (Connection con = ConnectionManager.getConnection(); PreparedStatement pstmt = con.prepareStatement(sql)) {
             setParameters(pstmt);
-
             return pstmt.executeUpdate();
-        } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
+        } catch (SQLException sqlException) {
+            throw new IllegalArgumentException("잘못된 SQL입니다.");
         }
     }
 
-    public void insert(String sql) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
+    public void insert(String sql) {
+        try (Connection con = ConnectionManager.getConnection(); PreparedStatement pstmt = con.prepareStatement(sql)) {
             setParameters(pstmt);
-
             pstmt.executeUpdate();
-        } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-
-            if (con != null) {
-                con.close();
-            }
+        } catch (SQLException sqlException) {
+            throw new IllegalArgumentException("잘못된 SQL입니다.");
         }
     }
 
